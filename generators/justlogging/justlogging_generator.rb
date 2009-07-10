d0837b33eeb0aa0d93f8accad70d7eaaade7a0ff
Robert Beekman <robertbeekman@noname.(none)>
2009-07-10 12:06:32 +0200

    fixed generator

---
+++ generators/justlogging/justlogging_generator.rb
@@ -0,0 +1,9 @@
+class JustloggingGenerator < Rails::Generator::Base
+
+  def manifest
+    record do |m|
+      m.directory 'config/initializers'
+      m.file 'justlogging.rb', 'config/initializers/justlogging.rb'
+    end
+  end
+end
--- generators/templates_generator.rb
+++
@@ -1,8 +0,0 @@
-class JustloggingGenerator < Rails::Generator::Base
-
-def manifest
-  record do |m|
-    m.directory 'config/initializers'
-    m.file 'justlogging.rb', 'config/initializers/justlogging.rb'
-  end
-end
